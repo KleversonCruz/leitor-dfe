5d55dd5b0b9160890976c626d2490d164b35e949
Kleverson Marinho <kleverson.marinho@outlook.com>
2022-03-08 23:56:58 -0300

    Commit 08-03-2022 23:56

--- dist/routes.js
+++ dist/routes.js
@@ -3,7 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.routes = void 0;
 const report_controller_1 = require("./controllers/report-controller");
 function routes(app) {
-    app.post('/report/details', report_controller_1.ReportController.generateReport);
+    app.post('/report', report_controller_1.ReportController.generateReport);
+    app.route('/').get(function (req, res) {
+        res.redirect('https://documenter.getpostman.com/view/16890150/UVsFx7vb');
+    });
 }
 exports.routes = routes;
 //# sourceMappingURL=routes.js.map
--- src/routes.ts
+++ src/routes.ts
@@ -2,5 +2,9 @@ import { Application } from 'express';
 import { ReportController } from './controllers/report-controller';
 
 export function routes(app: Application) {
-  app.post('/report/details', ReportController.generateReport);
+  app.post('/report', ReportController.generateReport);
+  
+  app.route('/').get(function (req, res) {
+    res.redirect('https://documenter.getpostman.com/view/16890150/UVsFx7vb');
+  });
 }
