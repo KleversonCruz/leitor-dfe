4446674058fc566aa36544ea13d7f338ee4e0261
Kleverson Marinho <kleverson.marinho@outlook.com>
2022-03-08 23:50:23 -0300

    Commit 08-03-2022 23:50

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,7 @@ const routes_1 = require("./routes");
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(fileUpload({
     createParentPath: true,
     limits: {
--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(
   fileUpload({
