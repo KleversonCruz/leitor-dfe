ecf4a93a2937014d7c0a5d9a8b986bb21b1878fb
Kleverson Marinho <kleverson.marinho@outlook.com>
2022-04-11 12:30:42 -0300

    add DockerFile

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReportController } from 'src/controllers/report.controller';
 import { ParseJsCsvService } from 'src/services/parse-js-csv.service';
 import { ParseXmlJsService } from 'src/services/parse-xml-js.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [ReportController],
   providers: [ParseXmlJsService, ParseJsCsvService],
 })
