ccd58abeb272d0000ff965add5d5cbbe1d7dd000
Kleverson Marinho <kleverson.marinho@outlook.com>
2022-05-22 00:10:37 -0300

    fix calculo de valor total

--- src/shared/utils/getTotalValue.ts
+++ src/shared/utils/getTotalValue.ts
@@ -2,7 +2,7 @@ import { ReportJsDto } from '../dtos/report-js.dto';
 
 export default function getTotalValue(reportJsDtos: ReportJsDto[]) {
   const itens = reportJsDtos.flatMap((x) => x.ide.vDocumento);
-  if (itens.length > 1) {
+  if (itens.length > 0) {
     const reducedArray = itens.reduce((accumulator, item) => {
       return accumulator + item;
     });
